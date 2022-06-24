6c55bc94db891230ffb6543bf3a7c8e79ea75209
user <shinage2@naver.com>
2022-06-25 03:04:10 +0900

    switch문

--- app/src/main/java/com/cookandroid/myfintech/StampActivity.java
+++ app/src/main/java/com/cookandroid/myfintech/StampActivity.java
@@ -3,37 +3,119 @@ package com.cookandroid.myfintech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class StampActivity extends AppCompatActivity {
-    ImageView stemp1,stemp2,stemp3,stemp4,stemp5,stemp6,stemp7,stemp8,stemp9,stemp10,stemp11,stemp12,stemp13,stemp14,stemp15,stemp16,stemp17,stemp18,stemp19,stemp20;
+    TextView count;
+    ArrayList<ImageView> imgArray = new ArrayList<ImageView>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stamp);
 
-        imgSet();
+        textSet();
+        arrayImgSet();
+        imgClickSet();
     }
-    private void imgSet(){
-        stemp1= findViewById(R.id.stamp1);
-        stemp2= findViewById(R.id.stamp2);
-        stemp3= findViewById(R.id.stamp3);
-        stemp4= findViewById(R.id.stamp4);
-        stemp5= findViewById(R.id.stamp5);
-        stemp6= findViewById(R.id.stamp6);
-        stemp7= findViewById(R.id.stamp7);
-        stemp8= findViewById(R.id.stamp8);
-        stemp9= findViewById(R.id.stamp9);
-        stemp10= findViewById(R.id.stamp10);
-        stemp11= findViewById(R.id.stamp11);
-        stemp12= findViewById(R.id.stamp12);
-        stemp13= findViewById(R.id.stamp13);
-        stemp14= findViewById(R.id.stamp14);
-        stemp15= findViewById(R.id.stamp15);
-        stemp16= findViewById(R.id.stamp16);
-        stemp17= findViewById(R.id.stamp17);
-        stemp18= findViewById(R.id.stamp18);
-        stemp19= findViewById(R.id.stamp19);
-        stemp20= findViewById(R.id.stamp20);
+
+    private void textSet() {
+        count = findViewById(R.id.count);
+    }
+
+    private void arrayImgSet(){
+        imgArray.add(findViewById(R.id.stamp1));
+        imgArray.add(findViewById(R.id.stamp2));
+        imgArray.add(findViewById(R.id.stamp3));
+        imgArray.add(findViewById(R.id.stamp4));
+        imgArray.add(findViewById(R.id.stamp5));
+        imgArray.add(findViewById(R.id.stamp6));
+        imgArray.add(findViewById(R.id.stamp7));
+        imgArray.add(findViewById(R.id.stamp8));
+        imgArray.add(findViewById(R.id.stamp9));
+        imgArray.add( findViewById(R.id.stamp10));
+        imgArray.add( findViewById(R.id.stamp11));
+        imgArray.add( findViewById(R.id.stamp12));
+        imgArray.add( findViewById(R.id.stamp13));
+        imgArray.add( findViewById(R.id.stamp14));
+        imgArray.add( findViewById(R.id.stamp15));
+        imgArray.add( findViewById(R.id.stamp16));
+        imgArray.add( findViewById(R.id.stamp17));
+        imgArray.add( findViewById(R.id.stamp18));
+        imgArray.add( findViewById(R.id.stamp19));
+        imgArray.add( findViewById(R.id.stamp20));
+    }
+    private void imgClickSet(){
+
+        for(int i=0;i<20;i++){
+            switch (i/4){
+                case 0:
+                    if(i%2==0) {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame29));
+                        });
+                    }
+                    else {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame30));
+                        });
+                    }
+                    break;
+                case 1:
+                    if(i%2==0) {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame33));
+                        });
+                    }
+                    else {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame36));
+                        });
+                    }
+                    break;
+                case 2:
+                    if(i%2==0) {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame37));
+                        });
+                    }
+                    else {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame40));
+                        });
+                    }
+                    break;
+                case 3:
+                    if(i==13-1){//13번째
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame56));
+                        });
+                    }
+                    else if(i%2==0) {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame37));
+                        });
+                    }
+                    else {
+                        int j=i;//for문의 i 인식 못함
+                        imgArray.get(j).setOnClickListener(view -> {
+                            imgArray.get(j).setImageDrawable(getResources().getDrawable(R.drawable.frame40));
+                        });
+                    }
+                    break;
+            }
+        }
     }
 }
