ef84e01d7beb0168fad4c7032a858fd123c403b2
user <shinage2@naver.com>
2022-06-25 00:40:03 +0900

    xml추가

--- app/src/main/java/com/cookandroid/myfintech/StampActivity.java
+++ app/src/main/java/com/cookandroid/myfintech/StampActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class StampActivity extends AppCompatActivity {
-
+    ImageView stemp1,stemp2,stemp3,stemp4,stemp5,stemp6,stemp7,stemp8,stemp9,stemp10,stemp11,stemp12,stemp13,stemp14,stemp15,stemp16,stemp17,stemp18,stemp19,stemp20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
