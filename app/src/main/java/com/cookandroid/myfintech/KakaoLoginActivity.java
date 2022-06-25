a134a5ce2a154eb1e5bc59598c7cbaea9832b01e
user <shinage2@naver.com>
2022-06-25 20:20:07 +0900

    Revert "KaKaoLogin수정"
    This reverts commit 5b3b4b68

---
+++ app/src/main/java/com/cookandroid/myfintech/KakaoLoginActivity.java
@@ -0,0 +1,24 @@
+package com.cookandroid.myfintech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class KakaoLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kakao_login);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://retumble.cafe24app.com/kakao"));
+        // intent.setPackage("com.android.chrome");   // 브라우저가 여러개 인 경우 콕 찍어서 크롬을 지정할 경우
+        startActivity(intent);
+
+        findViewById(R.id.img).setOnClickListener(view ->{
+            Intent i =new Intent(this,MainActivity.class);
+            startActivity(i);
+        });
+    }
+}
